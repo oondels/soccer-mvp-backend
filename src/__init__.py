27750711ae3933cd57150cd10da0f513a64cda17
Hendrius Dass <hendriusfelix.dev@gmail.com>
2025-06-18 16:51:53 -0300

    feat: initialize bcrypt for password hashing and load environment variables

--- src/__init__.py
+++ src/__init__.py
@@ -2,16 +2,22 @@ import os
 from flask import Flask
 from src.routes import register_routes
 from src.database.db import db
+from src.dependencies import bcrypt
+from dotenv import load_dotenv
+
 
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 DB_PATH = os.path.join(BASE_DIR, "database", "soccer_mvp.db")
 
 def create_app():
+    load_dotenv()
     app = Flask(__name__)
     
     app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_PATH}'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    
     db.init_app(app)
+    bcrypt.init_app(app)
     
     register_routes(app)
 
